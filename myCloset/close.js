9a679d7618e0a70c0719f34dcd2a0b5fe298ee06
soeun823 <bsoeun866@gmail.com>
2024-09-02 17:53:25 +0900

    add : 온도 슬라이드 바

--- myCloset/close.js
+++ myCloset/close.js
@@ -3,3 +3,11 @@ const $move_back = document.getElementById("back");
 $move_back.addEventListener("click", (i) => {
     window.location.href = './index.html';
 });
+
+const slider = document.getElementById("input_tmp");
+const output = document.getElementById("value");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
