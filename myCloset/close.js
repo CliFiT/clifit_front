eb1914763e43292b8942d459586de7400083965f
soeun823 <bsoeun866@gmail.com>
2024-09-02 21:33:04 +0900

    add : 버튼 클릭

--- myCloset/close.js
+++ myCloset/close.js
@@ -11,3 +11,47 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+//버튼 클릭시
+const $kindBtn = document.querySelectorAll(".kind button");
+const $seasonBtn = document.querySelectorAll(".season button");
+const $styleBtn = document.querySelectorAll(".style button");
+let kind ='', season ='',style ='';
+
+$kindBtn.forEach((clickBtn) => {
+    clickBtn.addEventListener("click", function(event) {
+        if (!kind) {
+            kind = clickBtn.textContent;
+            console.log(kind);
+            clickBtn.classList.add('active');
+            // if (style) {
+            //     $submitBtn.classList.add('active');
+            // }
+        }
+    });
+});
+$seasonBtn.forEach((clickBtn) => {
+    clickBtn.addEventListener("click", function(event) {
+        if (!season) {
+            season = clickBtn.textContent;
+            console.log(season);
+            clickBtn.classList.add('active');
+            // if (style) {
+            //     $submitBtn.classList.add('active');
+            // }
+        } 
+    });
+});
+$styleBtn.forEach((clickBtn) => {
+    clickBtn.addEventListener("click", function(event) {
+        if (!style) {
+            style = clickBtn.textContent;
+            console.log(style);
+            clickBtn.classList.add('active');
+            // if (style) {
+            //     $submitBtn.classList.add('active');
+            // }
+        } 
+    });
+});
+
